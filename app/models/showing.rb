a3ea778d06314938d0c1a74b0eafcfd30b1fbac3
Harvey M-A <hm00929@surrey.ac.uk>
2020-12-12 06:23:21 -0800

    Added scheduling validation for showings, swapped date format for views

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -1,12 +1,25 @@
+class ShowingValidator < ActiveModel::Validator
+  def validate(showing)
+    s = Showing.where(room: showing.room)
+    showings = s.where('(startTime BETWEEN ? AND ?) OR (finishTIme BETWEEN ? AND ?)', showing.startTime, showing.finishTime, showing.startTime, showing.finishTime)
+    puts "SHOWING TEST:"
+    puts showings
+    unless showings.count != 0
+      showing.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
 class Showing < ApplicationRecord
-  # ID, startTime, FK RoomID, FK FilmID  
+  # ID, startTime, finishTime, FK room, FK film  
 
   # Validations
   validates :startTime, presence: true
   validates :room, presence: true
   validates :film, presence: true
+  validate :validate_time # our custom validator
   
-
+  # might delete these
   def self.getCurrentShowings
     return currentShowings = Showings.all
   end
@@ -15,18 +28,18 @@ class Showing < ApplicationRecord
     return Showing.where('startTime > ?', Date.today)
   end
 
-
+  # Associations
   belongs_to :film
   belongs_to :room
 
-  # Check if a given interval overlaps this interval    
-  def overlaps?(other)
-    startTime <= other.finishTime && other.startTime <= finishTime
+  # This takes the current showing object and checks if it
+  # overlaps with any showing scheduled in the same room
+  def validate_time
+    s = Showing.where(room: room)
+    showings = s.where('(startTime BETWEEN ? AND ?) OR (finishTIme BETWEEN ? AND ?)', startTime, finishTime, startTime, finishTime)
+    unless showings.count == 0
+      errors.add(:base, "This conflicts with another showing!")
+    end
   end
-
-  # Return a scope for all interval overlapping the given interval, excluding the given interval itself
-  scope :overlapping, -> { (interval)
-    where("id <> ? AND startTime <= ? AND ? <= finishTime", interval.id, interval.finishTime, interval.startTime)
-  }
  
 end
